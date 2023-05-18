9a67ddce140559da45c8665bd917134cfbb7cfa2
Quang <phvuquang@gmail.com>
2023-05-18 15:09:39 +0700

    Adjustments to Dijkstra Algorithm

--- DijkstraAlgo/src/Dijkstra.java
+++ DijkstraAlgo/src/Dijkstra.java
@@ -2,77 +2,71 @@
 import java.util.*;
 
 public class Dijkstra {
-    public static List<Node> dijkstra(Map<String, List<Node>> graph, Node start, Node end){
-        // Create a set of settled nodes and an unordered set of unsettled nodes
-        Set<Node> settledNodes = new HashSet<>();
-        Set<Node> unsettledNodes = new HashSet<>();
+    public static int V = 5;
 
-        // Add the start node to the unsettled nodes set
-        unsettledNodes.add(start);
-        // While there are unsettled nodes
-        while (!unsettledNodes.isEmpty()) {
+    //function to find the vertices with shortest distance
+    //that does not include in the minimum spanning tree
+    public int minDistance(int dist[], Boolean sptSet[]){
+        int min = Integer.MAX_VALUE, min_index = -1;
 
-            // Find the node with the shortest distance from the start node
-            Node currentNode = Collections.min(unsettledNodes, (node1, node2) -> node1.distance - node2.distance);
-
-            // If the current node is null, then return an empty list
-            if (currentNode == null) {
-                return new ArrayList<>();
+        for (int v = 0; v < V; v++){
+            if (!sptSet[v] &&
+                dist[v] <= min){
+                min = dist[v];
+                min_index = v;
             }
+        }
+        return min_index;
+    }
+    public void printSolution(int dist[]){
+        System.out.println("Vertex \t Distance from source");
+        for (int i = 0; i < V; i++){
+            System.out.println(i + " \t" + dist[i]);
+        }
+    }
 
-            // Mark the current node as settled
-            settledNodes.add(currentNode);
+    public void dijkstra(int graph[][], int src){
+        //output array, holds all the distances from src to node i
+        int dist[] = new int[V];
 
-            // Remove the current node from the unsettled nodes set
-            unsettledNodes.remove(currentNode);
+        //array that holds all the nodes, will initially set as false
+        //will be true if the nodes are visited
+        Boolean sptSet[] = new Boolean[V];
 
-            // For each neighbor of the current node
-            for (Node neighbor : graph.get(currentNode.name)) {
+        //Initialize all distances as infinite and sptSet array as false
+        for (int i = 0; i < V; i++){
+            dist[i] = Integer.MAX_VALUE;
+            sptSet[i] = false;
+        }
 
-                // If the neighbor is not settled
-                if (!settledNodes.contains(neighbor)) {
+        //Distance of source vertex is set as 0
+        dist[src] = 0;
 
-                    // Calculate the new distance to the neighbor
-                    int newDistance = currentNode.distance + neighbor.distance;
+        //Find shortest path for all vertices
+        //Loop to V - 1 because the first vertex is already processed
+        for (int i = 0; i < V - 1; i++){
+            //Pick the minimum distance vertex from the set of unprocessed vertices
+            //u is equal to the first vertex in the first iteration
+            int u = minDistance(dist, sptSet);
 
-                    // If the new distance is shorter than the current distance to the neighbor
-                    if (newDistance < neighbor.distance) {
+            //mark the picked vertex as processed
+            sptSet[u] = true;
 
-                        // Update the neighbor's distance
-                        neighbor.distance = newDistance;
-
-                        // Add the neighbor to the unsettled nodes set
-                        unsettledNodes.add(neighbor);
-                    }
+            //update the distance value of adjacent vertices of the picked vertex
+            for (int v = 0; v < V; v++){
+                //Update dist[v] if vertex is not in sptSet
+                //and there is an edge from u to v
+                //and total weight of path from source to v
+                //through u is smaller than current dist[v]
+                if (!sptSet[v] &&
+                    graph[u][v] != 0 &&
+                    dist[u] != Integer.MAX_VALUE &&
+                    dist[u] + graph[u][v] < dist[v]){
+                    dist[v] = dist[u] + graph[u][v];
                 }
             }
         }
-        // Return the shortest path from the start node to the end node
-        return getShortestPath(start, end);
-    }
-
-    public static List<Node> getShortestPath(Node start, Node end) {
-
-        // Create a list to store the nodes in the shortest path
-        List<Node> shortestPath = new ArrayList<>();
-
-        // Start at the end node
-        Node currentNode = end;
-
-        // While the current node is not the start node
-        while (!currentNode.equals(start)) {
-
-            // Add the current node to the shortest path
-            shortestPath.add(currentNode);
-
-            // Set the current node to its predecessor
-            currentNode = currentNode.predecessor;
-        }
-
-        // Reverse the order of the shortest path
-        Collections.reverse(shortestPath);
-
-        // Return the shortest path
-        return shortestPath;
+        //output solution
+        printSolution(dist);
     }
 }
--- DijkstraAlgo/src/Main.java
+++ DijkstraAlgo/src/Main.java
@@ -2,22 +2,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a graph
-        Map<String, List<Node>> graph = new HashMap<>();
+        int[][] adjMatrix = {
+                {0, 6, 0, 1, 0},
+                {6, 0, 5, 2, 2},
+                {0, 5, 0, 0, 5},
+                {1, 2, 0, 0, 1},
+                {0, 2, 5, 1, 0}
+        };
 
-        // Add nodes to the graph
-        graph.put("A", Arrays.asList(new Node("B", 5), new Node("C", 2)));
-        graph.put("B", Arrays.asList(new Node("A", 5), new Node("C", 3)));
-        graph.put("C", Arrays.asList(new Node("A", 2), new Node("B", 3)));
+        Dijkstra dijkstra = new Dijkstra();
+        dijkstra.dijkstra(adjMatrix, 0);
 
-        // Find the shortest path from A to C
-        Node start = new Node("A");
-        Node end = new Node("C");
-        List<Node> shortestPath = Dijkstra.dijkstra(graph, start, end);
-
-        // Print the shortest path
-        for (Node node : shortestPath) {
-            System.out.print(node.name + " ");
-        }
     }
 }
